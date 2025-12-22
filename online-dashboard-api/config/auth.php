f5af86ba800500a5734a4450ab1c70d4cbf0ba03
Duggirala Akhil <akhil.duggirala@loqbox.com>
2025-12-22 11:15:41 +0530

    [main] Update OAuth

--- online-dashboard-api/config/auth.php
+++ online-dashboard-api/config/auth.php
@@ -116,7 +116,7 @@ return [
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
-    'oauth_token_uri' => 'http://127.0.0.1:8000/oauth/token',
+    'oauth_token_uri' => env('OAUTH_TOKEN_URI'),
 
     'reset_password_expiry_time_limit' => 5,
 
