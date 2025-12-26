224703447a28a5cc2623791f280fce1e1ef9820f
Duggirala Akhil <akhil.duggirala@loqbox.com>
2025-12-26 14:44:58 +0530

    [main] Revert the revert

--- online-dashboard-api/config/passport.php
+++ online-dashboard-api/config/passport.php
@@ -73,8 +73,10 @@ return [
     ],
 
     'password_client' => [
-        'id' => env('PASSPORT_PASSWORD_CLIENT_ID'),
-        'secret' => env('PASSPORT_PASSWORD_CLIENT_SECRET'),
+        'id' => env('PASSPORT_PASSWORD_CLIENT_ID')
+            ?: env('PASSWORD_GRANT_CLIENT_ID'),
+        'secret' => env('PASSPORT_PASSWORD_CLIENT_SECRET')
+            ?: env('PASSWORD_GRANT_CLIENT_SECRET'),
     ],
 
 ];
